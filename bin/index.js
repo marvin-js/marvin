632df5eca21eb463f4dc79a123590816c1082c2e
Carlos Alexandre Fuechter <alexandref93@gmail.com>
2018-02-04 17:18:34 -0200

    feat(core): add bin command

---
+++ bin/index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import program from 'commander';
+import idx from 'idx';
+
+import packageJSON from '../package.json';
+import { runFile } from '../utils/command';
+import * as libExternals from '../commands';
+
+program
+  .version(packageJSON.version)
+  .parse(process.argv);
+
+const fileToExecute = idx(program, _ => _.args[0]) || '.workflow';
+
+runFile(fileToExecute, libExternals);
+
+
