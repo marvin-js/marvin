f568b74ba0724e6fb286641441e45754c5cef3a4
Carlos Alexandre Fuechter <alexandref93@gmail.com>
2018-02-08 17:56:02 -0200

    feat(utils): add findVariables on regex

--- utils/regex.js
+++ utils/regex.js
@@ -1,5 +1,7 @@
-export const findAllCommand = /^(\$[a-z\s0-9\/\-]+?\s=\s)?([a-z\s0-9\/\-]+?)({[a-z\n\s0-9\/\-]*})?(\n|$)+?/gmi
+export const findAllCommand = /^(\$[a-z\s0-9\/\-]+?\s=\s)?([a-z\s0-9\/\-]+?)({[a-z\n\s0-9\/\-]*})?(\n|$)+?/gmi;
 
-export const isCommandWithSubCommand = /{([a-z\n\s0-9\/\-]*)}/gmi
+export const isCommandWithSubCommand = /{([a-z\n\s0-9\/\-]*)}/gmi;
 
-export const getNameWithSubCommand = /([a-z\s0-9\/\-]*)/
+export const getNameWithSubCommand = /([a-z\s0-9\/\-]*)/;
+ 
+export const findVariables = /^(?:\s?)(\$[a-z\s0-9\/\-]+?)(?:\s=?)/gmi;
