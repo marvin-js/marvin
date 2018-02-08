ac0bc9c6b91fcf861de9771cff446175c1b37b84
Carlos Alexandre Fuechter <alexandref93@gmail.com>
2018-02-08 12:35:55 -0200

    feat(utils): add regex to find all commands on file

---
+++ utils/regex.js
@@ -0,0 +1,5 @@
+export const findAllCommand = /^([a-z\s0-9\/\-]+?)({[a-z\n\s0-9\/\-]*})?(\n|$)+?/gmi
+
+export const isCommandWithSubCommand = /{([a-z\n\s0-9\/\-]*)}/gmi
+
+export const getNameWithSubCommand = /([a-z\s0-9\/\-]*)/
