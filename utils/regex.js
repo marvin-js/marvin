4a48cbde5f8db9bd5e0157326aabf52dcb08359e
Carlos Alexandre Fuechter <alexandref93@gmail.com>
2018-02-09 11:51:02 -0200

    improv(utils): adjusts on some regex

--- utils/regex.js
+++ utils/regex.js
@@ -1,7 +1,15 @@
-export const findAllCommand = /^(\$[a-z\s0-9\/\-]+?\s=\s)?([a-z\s0-9\/\-]+?)({[a-z\n\s0-9\/\-]*})?(\n|$)+?/gmi;
+export const findAllCommand = /^(\s+?(\$[a-z_0-9]+)(?:\s=\s))?([a-z\s0-9\/\-]+?)({[a-z\n\s0-9\/\-]*})?(\n|$)+?/gmi;
 
-export const isCommandWithSubCommand = /{([a-z\n\s0-9\/\-]*)}/gmi;
+export const isCommandWithSubCommand = /{([a-z\n\s0-9\/\-]*)}/mi;
 
-export const getNameWithSubCommand = /([a-z\s0-9\/\-]*)/;
+export const getSubCommand = /{([a-z\n\s0-9\/\-]*)}/gmi;
+
+export const replaceSubCommand = /{[a-z\n\s0-9\/\-]*}/gmi;
+
+export const getNameWithSubCommand = /(?:\s+?(\$[a-z_0-9]+)(?:\s=\s))?([a-z\s0-9\/\-]*)/gmi;
  
-export const findVariables = /^(?:\s?)(\$[a-z\s0-9\/\-]+?)(?:\s=?)/gmi;
+export const findVariables = /(\$[a-z_0-9]+)(?:\s=\s)/mi;
+
+export const getVariables = /(\$[a-z_0-9]+)(?:\s=\s)/mi;
+
+export const replaceVariables = /^\$[a-z_0-9]+\s=\s/gmi;
