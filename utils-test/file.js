409d540ba626c83bef9eabe57aaf0331185fc1d7
Carlos Alexandre Fuechter <alexandref93@gmail.com>
2018-02-16 00:03:17 -0200

    feat(lib): simplify write file

--- utils-test/file.js
+++ utils-test/file.js
@@ -2,11 +2,13 @@ import mkdirp from 'mkdirp';
 import fs from 'fs';
 import { dirname } from 'path';
 
-export function writeFile (path, contents, cb) {
-  mkdirp(dirname(path), function (err) {
-    if (err) return cb(err);
-
-    fs.writeFileSync(path, contents, 'utf8');
-    cb && cb();
+export function writeFile (path, contents) {
+  return new Promise((resolve, reject) => {
+    mkdirp(dirname(path), function (err) {
+      if (err) return reject(err);
+  
+      fs.writeFileSync(path, contents, 'utf8');
+      resolve()
+    });
   });
 }
