794d41440462e39382adf7f7f9204a7f6c8c8065
Carlos Alexandre Fuechter <alexandref93@gmail.com>
2018-02-21 14:42:31 -0300

    feat(commands): add interval

---
+++ commands/interval.js
@@ -0,0 +1,20 @@
+import { resolve } from "dns";
+
+export default function (opts = {}, milliseconds) {
+  return execute => {
+
+    let count = 0;
+
+    return new Promise(resolve => {
+      const id = setInterval(() => {
+        execute();
+        count = count + 1;
+
+        if (opts.count > 0 && count >= opts.count) {
+          clearInterval(id);
+          resolve();
+        }
+      }, milliseconds);
+    });
+  }
+}
