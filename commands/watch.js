14ca59b5a6667d13c63deee9c5f302c7b2575805
Carlos Alexandre Fuechter <alexandref93@gmail.com>
2018-02-21 23:36:25 -0300

    feat(commands): add watch

---
+++ commands/watch.js
@@ -0,0 +1,9 @@
+import chokidar from 'chokidar';
+
+export default function (opts, path) {
+  return execute => {
+    chokidar.watch(path).on('all', (event, path) => {
+      execute(path);
+    });
+  }
+}
