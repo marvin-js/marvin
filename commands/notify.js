c8e7b15091049b3312e2cb9e4c8a202523e4fc3c
Carlos Alexandre Fuechter <alexandref93@gmail.com>
2018-02-21 22:11:37 -0300

    feat(commands): add notify

---
+++ commands/notify.js
@@ -0,0 +1,14 @@
+import notifier from 'node-notifier';
+
+export default async function (opts, ...args) {
+  return new Promise(resolve => {
+    notifier.notify({
+      title: 'worlflow',
+      message: args.join(''),
+      wait: false,
+      reply: opts.input || false,
+    }, (err, response, metadata) => {
+      resolve(metadata.activationValue);  
+    });
+  })
+};
