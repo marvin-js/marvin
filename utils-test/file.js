83185b887e94f0e33b42ea6df2e4b8dff610192d
Carlos Alexandre Fuechter <alexandref93@gmail.com>
2018-02-22 00:47:19 -0300

    feat(utils-test): add read file

--- utils-test/file.js
+++ utils-test/file.js
@@ -12,3 +12,12 @@ export function writeFile (path, contents) {
     });
   });
 }
+
+export function readFile (inputFile, options = {}) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(inputFile, 'utf8', function(err, content) {
+      if (err) reject(err);
+      resolve(content);
+    });
+  });
+}
