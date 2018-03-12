bfdbdd2b8a6d573ba7ea6adc5852c35681292bd0
Carlos Alexandre Fuechter <alexandref93@gmail.com>
2018-03-12 14:22:34 -0300

    bug(helpers/repeat): adjusts when the params is only one array

--- src/commands/each.js
+++ src/commands/each.js
@@ -2,10 +2,9 @@ import idx from 'idx';
 import { repeat } from '../helpers';
 
 export default (opts, ...items) => {
-
   if (items.length === 1) {
     const value = idx(items, _ => items[0]);
-    if (typeof value === 'array') {
+    if (typeof value === 'array' || value.length > 0) {
       return repeat(value);
     }
   }
