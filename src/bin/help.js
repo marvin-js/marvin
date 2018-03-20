2c67bc8313cb86bb0c9980629a8bff16b827017f
Carlos Alexandre Fuechter <alexandref93@gmail.com>
2018-03-20 10:59:49 -0300

    refactor(help): simply the content of help

--- src/bin/help.js
+++ src/bin/help.js
@@ -1,22 +1,16 @@
 import chalk from 'chalk';
 
-export default () => {
-  console.log();
-  console.log();
-  console.log('  How use the Marvin, step-by-step:');
-  console.log();
-  console.log();
-  console.log(`    ${chalk.bgRed(' 1. ')}: Create um file with name .marvin and these content below`);
-  console.log();
-  console.log('          $content = watch ./src/file --async {');
-  console.log('            log file changed');
-  console.log('          }');
-  console.log();
-  console.log();
-  console.log(`    ${chalk.bgRed(' 2. ')}: Run the file`);
-  console.log();
-  console.log('          marvin');
-  console.log();
-  console.log();
-  console.log();
-};
+export default () => console.log(`
+  How use the Marvin, step-by-step:
+
+  ${chalk.bgRed(' 1. ')}: Create um file with name .marvin and these content below
+    $content = watch ./src/file --async {
+      log file changed
+    }
+
+  ${chalk.bgRed(' 2. ')}: Run the file
+
+    marvin
+
+
+`);
